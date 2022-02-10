8ec71d36d9e1c63ab1630c99287dd4b2c258f28f
Julius Madsen <2330051117@protonmail.com>
2022-02-10 16:48:44 +0100

    01_04 task done

---
+++ week_01/01_04_data_structures_(dictionaries)/01_04_python.py
@@ -0,0 +1,10 @@
+
+#   A date of the form 8-MAR-85 includes the name of the month, which must be translated to a number.
+#   Create a dict suitable for decoding month names to numbers.
+#   use string operations to split the date into 3 items using the '-' character.
+#   Translate the month and return a tuple.
+#   In the end we have a date 'dd-MMM-yy' format and we get a new string 'd-m-y'
+
+months = {'JAN' : '1', 'FEB' : '2', 'MAR' : '3', 'APR' : '4', 'MAY' : '5', 'JUN' : '6', 'JUL' : '7', 'AUG' : '8', 'SEP' : '9', 'OCT' : '10', 'NOV' : '11', 'DEC' : '12'}
+user_input = input("Enter a date in the format 'dd-MMM-yy' ex. '8-MAR-85': ")
+print('-'.join([months.get(i, i) for i in user_input.split('-')]))
