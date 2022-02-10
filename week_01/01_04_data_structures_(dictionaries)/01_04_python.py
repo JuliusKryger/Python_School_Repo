cdf39d262e82343c088d7671f63a50efe3ddf0b3
Julius Madsen <2330051117@protonmail.com>
2022-02-10 17:04:48 +0100

    01_04 comments tried some errorhandling

--- week_01/01_04_data_structures_(dictionaries)/01_04_python.py
+++ week_01/01_04_data_structures_(dictionaries)/01_04_python.py
@@ -6,5 +6,9 @@
 #   In the end we have a date 'dd-MMM-yy' format and we get a new string 'd-m-y'
 
 months = {'JAN' : '1', 'FEB' : '2', 'MAR' : '3', 'APR' : '4', 'MAY' : '5', 'JUN' : '6', 'JUL' : '7', 'AUG' : '8', 'SEP' : '9', 'OCT' : '10', 'NOV' : '11', 'DEC' : '12'}
-user_input = input("Enter a date in the format 'dd-MMM-yy' ex. '8-MAR-85': ")
-print('-'.join([months.get(i, i) for i in user_input.split('-')]))
+
+user_input = input("Enter a date in the format 'dd-MMM-yy' ex. '8-MAR-85': ")           # Takes user input and then saves it to our variable.
+try:
+    print('-'.join([months.get(i, i) for i in user_input.split('-')]))
+except ValueError:
+    print("Error: must be in 'dd-MMM-yy' format. ")
